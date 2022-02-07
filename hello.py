86176dff716ceb1b372683506306f1ff3c210bda
bramlenting1234 <99068035@mydavinci.nl>
2022-02-07 12:34:42 +0100

    hello py met tkinter

---
+++ hello.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from typing import Sized
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+buiten_scherm = tk.Tk()
+buiten_scherm.geometry("200x200")
+buiten_scherm.configure(bg="gold")
+
+binnen_scherm = tk.Label(buiten_scherm,padx=20,pady=30,fg="white",bg="red",text="Hello Tkinter !",font=(None, 15))
+
+
+binnen_scherm.pack()
+buiten_scherm.mainloop()
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
